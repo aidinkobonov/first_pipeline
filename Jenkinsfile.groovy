2d0d1d7cb899603ce3aa55ae2eff7d59081461f7
aidinkobonov <aidinkobonov@gmail.com>
2019-05-02 20:01:51 -0500

    added new steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,4 +5,15 @@ node {
     stage("Webserver Install"){
         sh 'ssh ec2-user@54.171.211.32 sudo yum install httpd -y'
     }
+     stage("Index file"){
+        sh 'scp index.html ec2-user@54.171.211.32:/tmp'
+    }
+    stage("Copy index file"){
+        sh 'ssh ec2-user@54.171.211.32 "sudo mv /tmp/index.html  /var/www/html/index.html"'
+    }
+    stage("Restart Webserver"){
+        sh 'ssh ec2-user@54.171.211.32 sudo systemctl restart httpd'
+
+    }
 }
+
