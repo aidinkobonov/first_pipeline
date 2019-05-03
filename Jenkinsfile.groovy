35ace087007c04dff45229e7204475f3f26f382d
aidinkobonov <aidinkobonov@gmail.com>
2019-05-02 20:41:12 -0500

    fixed quotes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,16 +5,15 @@ node {
         git 'git@github.com:aidinkobonov/cool_website.git'
     }
     stage("Webserver Install"){
-        sh "ssh ec2-user@${ENV} sudo yum install httpd -y"
+        sh 'ssh ec2-user@${ENV} sudo yum install httpd -y'
     }
      stage("Index file"){
-        sh "scp index.html ec2-user@${ENV}:/tmp"
+        sh 'scp index.html ec2-user@${ENV}:/tmp'
     }
     stage("Copy index file"){
-        sh "ssh ec2-user@${ENV} sudo mv /tmp/index.html  /var/www/html/index.html"
-    }
+        sh 'ssh ec2-user@${ENV} "sudo mv /tmp/index.html  /var/www/html/index.html" '
     stage("Restart Webserver"){
-        sh "ssh ec2-user@${ENV} sudo systemctl restart httpd"
+        sh 'ssh ec2-user@${ENV} sudo systemctl restart httpd'
 
     }
 }
