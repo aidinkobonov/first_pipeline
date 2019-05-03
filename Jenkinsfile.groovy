e06b07636409e0d5fc0537704a0ed736b4cb57ec
aidinkobonov <aidinkobonov@gmail.com>
2019-05-02 19:55:45 -0500

    fixed quotes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,6 +3,6 @@ node {
         git 'git@github.com:aidinkobonov/cool_website.git'
     }
     stage("Webserver Install"){
-        sh "ssh ec2-user@54.171.211.32 sudo yum install httpd -y"
+        sh 'ssh ec2-user@54.171.211.32 sudo yum install httpd -y'
     }
 }
