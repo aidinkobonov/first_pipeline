df844d53e1592eab0e3a89d2947e520a8727b358
aidinkobonov <aidinkobonov@gmail.com>
2019-05-02 19:55:02 -0500

    installed httpd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,4 +2,7 @@ node {
     stage("Pull Repo"){
         git 'git@github.com:aidinkobonov/cool_website.git'
     }
+    stage("Webserver Install"){
+        sh "ssh ec2-user@54.171.211.32 sudo yum install httpd -y"
+    }
 }
